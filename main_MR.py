db14906ac79c75dd14a4bd4dea920c293e53edb1
milind <milindchaudhary80@gmail.com>
2022-08-12 12:36:34 +0200

    Add function to display text inside the button

--- main_MR.py
+++ main_MR.py
@@ -80,3 +80,11 @@ def p2p_dist(p1,p2):
     x2,y2 = p2
     
     return( ( (x1 - x2)**2 + (y1 - y2)**2 )**0.5 )
+
+# to get the text on the button
+def clickText():
+    font = pygame.font.Font('freeansbold.ttf', 12)
+    text = font.render('CLICK HERE', antialias= True, color= WHITE)
+    textRect = text.get_rect()
+    textRect.center = (75, 495)
+    screen.blit(text, textRect)
