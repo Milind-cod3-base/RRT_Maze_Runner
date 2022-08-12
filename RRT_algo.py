87bc2e04f2db4e6f1f3e895209212f1b4d005f75
milind <milindchaudhary80@gmail.com>
2022-08-12 09:57:28 +0200

    Create RRT dummy function

---
+++ RRT_algo.py
@@ -0,0 +1,9 @@
+"""
+This module contains the RRT algorithm
+for path finding autonomous vehicles / robots.
+
+This module will be imported in main_MR.py
+"""
+
+def rrt():   # function to contain RRT algo
+    pass
