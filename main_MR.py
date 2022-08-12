79f14deb08e27ce5f1b6af5b843b054a50e2ee05
milind <milindchaudhary80@gmail.com>
2022-08-12 10:12:24 +0200

    Set colors and game parameters

--- main_MR.py
+++ main_MR.py
@@ -4,3 +4,19 @@ import time
 from random import randint
 
 pygame.init()   # Initialize all imported pygame modules
+
+# setting colors
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+
+# setting game parameters
+gameX = 20
+gameY = 40
+gameWidth = 440  # window width
+gameHeight = 400  # window height
+gameBorder = 3    # window border width
+
+
