b0237e9f25ebd5a0adfac5fac7c0cdf8ee72ca99
milind <milindchaudhary80@gmail.com>
2022-08-12 10:30:51 +0200

    Set custom color and display

--- main_MR.py
+++ main_MR.py
@@ -11,12 +11,18 @@ BLUE = (0,0,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
+color_custom_1 = (10,145,80)  # creating a custom color
 
 # setting game parameters
+screen = pygame.display.set_mode([500,500]) # gives a surface
 gameX = 20
 gameY = 40
 gameWidth = 440  # window width
 gameHeight = 400  # window height
 gameBorder = 3    # window border width
 
+# screen background setting
+screen.fill(WHITE)
+
+INT_MAX = 100000000000000
 
